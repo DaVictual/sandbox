ddd5d919695b205e14375e316a495330af7dce20
DaVictual <victualnl@gmail.com>
2022-10-11 15:24:48 -0400

    version 2 of lesson1 project

--- src/lesson1.java
+++ src/lesson1.java
@@ -1,5 +1,13 @@
-public class Main {
+//Author: Raul Alexandru Dimulescu
+//Date Created: 10/11/2022
+public class lesson1 {
     public static void main(String args[]){
-        System.out.println("Hello World");
+        System.out.println("From: Raul Alexandru Dimulescu");
+        System.out.println("Address: My PC");
+        System.out.println("Date: 10/11/2022");
+        System.out.println("");
+        System.out.println("To: Mr. X");
+        System.out.println("");
+        System.out.println("Message: Help! I'm trapped inside a computer!");
     }
 }
